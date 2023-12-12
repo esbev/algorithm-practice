69b9bd7abc5b358bada795a7255f69ae5a50b33a
Eric <esbev@hotmail.com>
2023-12-12 16:12:00 -0600

    log even numbers

---
+++ bootcamp/04-log-even-nums.js
@@ -0,0 +1,12 @@
+// Write code to print all even numbers from 0 to `num`
+// Assume `num` will be a positive number
+
+var logEvenNums = function(num) {
+    for (i=0; i<=num; i+=2) {
+        console.log(i);
+    }
+};
+
+console.log(logEvenNums(22));
+console.log(logEvenNums(23));
+
