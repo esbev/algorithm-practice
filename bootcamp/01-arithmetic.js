a91306738c059650b0df4f378f78c2fc4f6cb3d6
Eric <esbev@hotmail.com>
2023-12-12 16:02:19 -0600

    first commit

---
+++ bootcamp/01-arithmetic.js
@@ -0,0 +1,22 @@
+// Fill in the body of each function with the code required
+
+var add = function(num1, num2) {
+    return num1+num2;
+};
+
+var subtract = function(num1, num2) {
+    return num1-num2;
+};
+
+var multiply = function(num1, num2) {
+    return num1*num2;
+};
+
+var divide = function(num1, num2) {
+    return num1/num2;
+};
+
+console.log(add(10, 24)); 
+console.log(subtract(100, 91)); 
+console.log(multiply(7, 70)); 
+console.log(divide(33, 11)); 
