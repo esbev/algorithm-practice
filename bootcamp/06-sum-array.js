88ebeb590cb885e504f5f521fc126f863d6d6188
Eric <esbev@hotmail.com>
2023-12-13 19:59:23 -0600

    countdown and sumarray

---
+++ bootcamp/06-sum-array.js
@@ -0,0 +1,11 @@
+// Write code to add all the numbers in `arr` and return the total
+
+var sumArray = function(arr) {
+    let result = 0;
+    
+    for (i = 0; i < arr.length; i++ ){
+        result += arr[i];
+    }
+    
+    return result;
+};
